9b5f61c7135e647a73a62c38959c6884411fa74a
Rueben hodges <HodgesR@umich.edu>
2021-04-15 23:18:19 -0400

    doesnt work yet

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 public class testSelectionSort {
 	@Test
 	public void test() {
-		testPositive();
-		testNegative();
+		//testPositive();
+		//testNegative();
 		testMixed();
 		testDuplicates();
 	}
@@ -40,12 +40,42 @@ public class testSelectionSort {
     }
 
     public void testMixed(){
+			int[] arr = new int[5];
+			arr[0] = -8;
+			arr[1] = -9;
+			arr[2] = -7;
+			arr[3] = 10;
+			arr[4] = 0;
 
+			int[] Sortedarr = new int[5];
+			Sortedarr[0] = -9;
+			Sortedarr[1] = -8;
+			Sortedarr[2] = -7;
+			Sortedarr[3] = 0;
+			Sortedarr[4] = 10;
+
+			SelectionSort tempSort= new SelectionSort();
+			assertArrayEquals(tempSort.basicSelectionSort(arr),Sortedarr);
         /** Test data contains with both positive, negative and zeros **/
     }
 
     public void testDuplicates(){
+			int[] arr = new int[5];
+			arr[0] = -8;
+			arr[1] = -9;
+			arr[2] = 10;
+			arr[3] = 10;
+			arr[4] = 0;
 
+			int[] Sortedarr = new int[5];
+			Sortedarr[0] = -9;
+			Sortedarr[1] = -8;
+			Sortedarr[2] = 0;
+			Sortedarr[3] = 10;
+			Sortedarr[4] = 10;
+
+			SelectionSort tempSort= new SelectionSort();
+			assertArrayEquals(tempSort.basicSelectionSort(arr),Sortedarr);
         /** Test data contains duplicates **/
     }
 
